1d6dfc2450bb50f6fca55475d0a23eab6318ca3b
Batman <bgmyhan@gmail.com>
2020-04-19 15:11:07 -0700

    working random password generator. Needs to be linked to page

--- script.js
+++ script.js
@@ -1,38 +1,87 @@
 // Isolating random Letter;Symbol;Number///
 
-// prompt 
+// prompts for PassswordLength | UpperCaseLetters |LowerCaseLetters| Symbols//
 
-// var generatedPassword = prompt("Please decide how long you want your password to be"('generate'));
-function requestLength(){
-  var requestedLength = prompt("Please input how long your password will be.");
+var requestedPasswordLength = generate.addEventListener("click", function(){
+  prompt("Please decide how long you want your password to be using only numbers.")
+}
+)
+var requestedUpperLetters = generate.addEventListener("click", function(){
+  confirm("Do You Want To Include Upper Case Letters?")
+}
+)
+var requestLowerLetters = generate.addEventListener("click", function(){
+  confirm("Do You Want To Include Lower Case Letters?")
+}
+)
+var requestSymbols = generate.addEventListener("click", function(){confirm("Do You Want To Include Symbols?")
+}
+)
+var requestNumbers = generate.addEventListener("click", function(){confirm("Do You Want To Include Numbers?")
+}
+)
+
+
+const randomFunc = {
+  lower: requestLowerLetters,
+  upper: requestUpperLetters,
+  number: requestNumbers,
+  symbol: requestSymbols,
+
+ };
+
+generate.addEventListener("click", () => {
+  var userRequestedlength = requestPasswordLength.value;
+  var userRequestedLowerLetters = requestLowerLetters.checked;
+  var userRequestedUpperLetters = requestUpperLetters.checked;
+  var userRequestedNumbers = requestNumbers.checked;
+  var userRequestedSymbols = requestSymbols.checked
+ 
+
+  // Generating Full Password //
+  generatedPassword.innerText = generatePassword( userRequestedlength,
+  userRequestedLowerLetters,
+  userRequestedUpperLetters,
+  userRequestedNumbers,
+  userRequestedSymbols
+  );
+});
+
+// Generate Password Function
+function finalPassword(lower, upper, number, symbol, length) {
+
+  let generatedPassword = "";
+  const typescount = lower + upper + number + symbol;
+
+  const fullArray = [{lower}, {upper}, {number}, {symbol}].filter (item => Objectvalues(item)[0]);
+
+ for( i=0; i < length; i += typescount){
+   fullArray.forEach(type => {
+     const funcName = Object.keys(type)[0];
+
+     generatePassword += randomFunc[funcName]();
+   });
+
+ }
+  var finalPassword = generatePassword.slice(0, length);
+}
+
+
+var possibleUpperLetters =
+"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var possibleLowerLetters =
+"abcdefghijklmnopqrstuvwxyz"
+var possibleSymbols =
+"!@#$%^&*()/";
+var possibleNumbers =
+"0123456789";
+
+var randomNumbers = possibleNumbers[Math.floor(Math.random()*10)];
+var randomSymbols = possibleSymbols[Math.floor(Math.random()*10)];
+var randomUpperLetters = possibleUpperLetters[Math.floor(Math.random()*10)];
+var randomLowerLetters = possibleLowerLetters[Math.floor(Math.random()*10)];
+
 // }
-// var requestedLength = document.getElementById("generate");
-// var requestedSymbols = document.getElementById("generate");
-// var RequestedUpperLetters = document.getElementById("generate");
-// var RequestedLowerLetters = document.getElementById("generate");
-
-
-// function requestedLength.addEventListener('click', () => ) {
-//   passwordLength = requestedLength.value;
-//   console.log(length)
-// }
-
-
-// var possibleUpperLetters =
-// "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-// var possibleLowerLetters =
-// "abcdefghijklmnopqrstuvwxyz"
-// var possibleSymbols =
-// "!@#$%^&*()/";
-// var possibleNumbers =
-// "0123456789";
-
-// var randomNumbers = possibleNumbers[Math.floor(Math.random()*10)];
-// var randomSymbols = possibleSymbols[Math.floor(Math.random()*10)];
-// var randomUpperLetters = possibleUpperLetters[Math.floor(Math.random()*10)];
-// var randomLowerLetters = possibleLowerLetters[Math.floor(Math.random()*10)];
-
-// // }
 // console.log("Numbers", randomNumbers);
 // console.log("symbols", randomSymbols);
 // console.log(randomUpperLetters);
