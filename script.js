40818fb95c24a9f28afe7aa4ae87e6e12b206f7e
Batman <bgmyhan@gmail.com>
2020-04-18 23:56:02 -0700

    Initial repo of files

---
+++ script.js
@@ -0,0 +1,39 @@
+// Isolating random Letter;Symbol;Number///
+
+// prompt 
+
+// var generatedPassword = prompt("Please decide how long you want your password to be"('generate'));
+function requestLength(){
+  var requestedLength = prompt("Please input how long your password will be.");
+// }
+// var requestedLength = document.getElementById("generate");
+// var requestedSymbols = document.getElementById("generate");
+// var RequestedUpperLetters = document.getElementById("generate");
+// var RequestedLowerLetters = document.getElementById("generate");
+
+
+// function requestedLength.addEventListener('click', () => ) {
+//   passwordLength = requestedLength.value;
+//   console.log(length)
+// }
+
+
+// var possibleUpperLetters =
+// "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// var possibleLowerLetters =
+// "abcdefghijklmnopqrstuvwxyz"
+// var possibleSymbols =
+// "!@#$%^&*()/";
+// var possibleNumbers =
+// "0123456789";
+
+// var randomNumbers = possibleNumbers[Math.floor(Math.random()*10)];
+// var randomSymbols = possibleSymbols[Math.floor(Math.random()*10)];
+// var randomUpperLetters = possibleUpperLetters[Math.floor(Math.random()*10)];
+// var randomLowerLetters = possibleLowerLetters[Math.floor(Math.random()*10)];
+
+// // }
+// console.log("Numbers", randomNumbers);
+// console.log("symbols", randomSymbols);
+// console.log(randomUpperLetters);
+// console.log(randomLowerLetters);
