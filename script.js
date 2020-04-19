9a7df57cdf3a46c5aacf20e1e10910963f47aa6e
Batman <bgmyhan@gmail.com>
2020-04-19 16:51:49 -0700

    updated

--- script.js
+++ script.js
@@ -2,54 +2,64 @@
 
 // prompts for PassswordLength | UpperCaseLetters |LowerCaseLetters| Symbols//
 
-var requestPasswordLength = generate.addEventListener("click", function(){
-  prompt("Please decide how long you want your password to be using only numbers.")
-}
+var requestPasswordLength = generate.addEventListener(
+  "click", function(){
+  prompt(
+    "Please decide how long you want your password to be using only numbers."
+    )
+  }
 )
-var requestUpperLetters = generate.addEventListener("click", function(){
-  confirm("Do You Want To Include Upper Case Letters?")
-}
+var requestUpperLetters = generate.addEventListener(
+  "click", function(){
+    confirm(
+    "Do You Want To Include Upper Case Letters?"
+    )
+  }
 )
-var requestLowerLetters = generate.addEventListener("click", function(){
-  confirm("Do You Want To Include Lower Case Letters?")
-}
+var requestLowerLetters = generate.addEventListener(
+  "click", function(){
+    confirm(
+    "Do You Want To Include Lower Case Letters?"
+    )
+  }
 )
-var requestSymbols = generate.addEventListener("click", function(){confirm("Do You Want To Include Symbols?")
-}
+var requestSymbols = generate.addEventListener(
+  "click", function(){
+    confirm(
+    "Do You Want To Include Symbols?"
+    )
+  }
+) 
+var requestNumbers = generate.addEventListener(
+  "click", function(){
+    confirm(
+    "Do You Want To Include Numbers?"
+    )
+  }
 )
-var requestNumbers = generate.addEventListener("click", function(){confirm("Do You Want To Include Numbers?")
-}
-)
+var finalPassword = document.getElementById("password");
 
 const randomFunc = {
   upper: requestUpperLetters,
   lower: requestLowerLetters,
   symbol: requestSymbols,
-  number: requestNumbers
-
+  number: requestNumbers,
  }
-
-// generate.addEventListener("click", () => {
-//   var userRequestedlength = requestPasswordLength;
-//   var userRequestedLowerLetters = requestLowerLetters.true;
-//   var userRequestedUpperLetters = requestUpperLetters.true;
-//   var userRequestedNumbers = requestNumbers.true;
-//   var userRequestedSymbols = requestSymbols.true
  
-
-  // Generating Full Password //
-  generatedPassword.innerText = generatePassword( userRequestedlength,
-  userRequestedLowerLetters,
-  userRequestedUpperLetters,
-  userRequestedNumbers,
-  userRequestedSymbols
-  );
-});
-
 // Generate Password Function
+    var generatedPassword = "";
+    password.innerText = (generatedPassword +
+    [requestPasswordLength,
+    requestLowerLetters,
+    requestUpperLetters,
+    requestNumbers,
+    requestSymbols]
+  );
+
+
 function finalPassword(lower, upper, number, symbol, length) {
 
-  let generatedPassword = "";
+  
   const typescount = lower + upper + number + symbol;
 
   const fullArray = [{lower}, {upper}, {number}, {symbol}].filter (item => Objectvalues(item)[0]);
@@ -58,11 +68,11 @@ function finalPassword(lower, upper, number, symbol, length) {
    fullArray.forEach(type => {
      const funcName = Object.keys(type)[0];
 
-     generatePassword += randomFunc[funcName]();
+     finalPassword += randomFunc[funcName]();
    });
 
  }
-  var finalPassword = generatePassword.slice(0, length);
+  var finalPassword = finalPassword.slice(0, length);
 }
 
 
@@ -80,3 +90,4 @@ var randomSymbols = possibleSymbols[Math.floor(Math.random()*10)];
 var randomUpperLetters = possibleUpperLetters[Math.floor(Math.random()*10)];
 var randomLowerLetters = possibleLowerLetters[Math.floor(Math.random()*10)];
 
+console.log(finalPassword)
