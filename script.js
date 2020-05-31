54352693f64db3246ef5f93649ea79b1d44f12df
Batman <bgmyhan@gmail.com>
2020-05-31 14:48:17 -0700

    Functional Password Generator Important

--- script.js
+++ script.js
@@ -1,53 +1,60 @@
+// Declaring variables needed to run Password Generator //
 var generateBtn = document.getElementById("generateBtn");
-// Password Generator Click //
 var myPassword =  generateBtn.addEventListener("click", function() {
-  // Creating variables from user input //
+  // Creating variables from user input elements//
   var reqPassLength = document.getElementById("reqPassLength");
-  var reqNum = document.getElementById("reqNum");
-  var reqSym = document.getElementById("reqSym");
-  var reqLLetter = document.getElementById("reqLLetter");
-  var reqULetter = document.getElementById("reqULetter");
+  var reqNum = document.getElementById("reqNum").checked;
+  var reqSym = document.getElementById("reqSym").checked;
+  var reqLLetter = document.getElementById("reqLLetter").checked;
+  var reqULetter = document.getElementById("reqULetter").checked;
+  // Creating parameters for users options //
   var lLetters = "abcdefghijklmnopqrstuvwxyz";
   var ULetters ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var syms = "!@#$%^&&*()";
   var num = "1234567890";
   var length = reqPassLength.value
   var pass = "";
-
-  // Generating password based on user variables //
-
-  // Generating individual symbols for password //
-
-
- // Creating Password based upon user preferences//
-  var length = parseInt(reqPassLength.value);
-  var lowerChar = reqLLetter.checked;
-  var upperChar = reqULetter.checked;
-  var symChar = reqSym.checked;
-  var numChar = reqNum.checked;
-
-
-  for (i=0; i<length; i++){
-    if (lowerChar){
-      llChar = lLetters.charAt(Math.floor(Math.random() * lLetters.length )+1)
-      pass = pass.concat(llChar)
-      console.log(pass)
+  var password = "";
+  // Requesting permissions from user checked elements//
+if (reqNum){
+  for (i=0; i<=length; i++){
+    var ranNumGen = function getRandomNum(){
+      return num.charAt(Math.floor(Math.random() * num.length ));
     }
-    if (upperChar){
-      ulChar = ULetters.charAt(Math.floor(Math.random() * ULetters.length )+1)
-      pass = pass.concat(ulChar)
-      console.log(pass)
+  pass += ranNumGen()
+  }
+}
+if (reqSym){
+  for (i=0; i<=length; i++){
+    var getSym = function getRandomSym(){
+      return syms.charAt(Math.floor(Math.random() * syms.length ));
     }
-    if (symChar){
-      sChar = syms.charAt(Math.floor(Math.random() * syms.length )+1)
-      pass = pass.concat(sChar)
-      console.log(pass)
+  pass += getSym()
+  }
+}
+if (reqLLetter){
+  for (i=0; i<=length; i++){
+    function getRandomLLetters(){
+      return lLetters.charAt(Math.floor(Math.random() * lLetters.length )+1)
     }
-    if (numChar){
-      nChar = num.charAt(Math.floor(Math.random() * num.length )+1)
-      pass = pass.concat(nChar)
-      console.log(pass)
+  pass += getRandomLLetters()
+  }
+}
+if (reqULetter){
+  for (i=0; i<=length; i++){
+    function getRandomULetters(){
+      return ULetters.charAt(Math.floor(Math.random() * ULetters.length )+1)
     }
-      }
-
-document.getElementById("password").innerText = pass;
+  pass += getRandomULetters()
+  }
+}
+// creating password from permissions arranged symbiotically structured aka p.a.s.s. //
+for (i=0; i<length; i++){
+  password += pass.charAt(Math.floor(Math.random() * pass.length ))
+  }
+// Printing password to password generator//
+document.getElementById("password").innerText = password;
+// Ending click function//
+});
+// Please enjoy your password built by Brandon Myhan//
+// And be sure to look out, Coming this Winter: version 2 with increased security features and new user interface//
